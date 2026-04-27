417e24b4459b1fc73a459cac0b494742b12d6929
murillomaciel <murillo.maciel@rede.ulbra.br>
2026-04-27 11:51:34 -0300

    Fix collision on the back of the enemy and add lives to big enemy

--- tilemania/Assets/Scripts/Bullet.cs
+++ tilemania/Assets/Scripts/Bullet.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
@@ -7,30 +6,30 @@ public class Bullet : MonoBehaviour
     private Rigidbody2D _myRigidBody2D;
     private PlayerMovement _player;
     private float _xSpeed;
-    
-    void Start()
+
+    private void Start()
     {
         _myRigidBody2D = GetComponent<Rigidbody2D>();
         _player = FindFirstObjectByType<PlayerMovement>();
         _xSpeed = _player.transform.localScale.x * bulletSpeed;
     }
 
-    void Update()
+    private void Update()
     {
         _myRigidBody2D.linearVelocityX = _xSpeed;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy"))
         {
             Destroy(other.gameObject);
         }
-        Destroy(gameObject);
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        Destroy(gameObject, 1F);
+        else if (other.gameObject.CompareTag("BigEnemy"))
+        {
+            var bigEnemy = FindAnyObjectByType<BigEnemy>();
+            bigEnemy.OnBulletHit();
+        }
+        Destroy(gameObject, 0.03F);
     }
 }
--- tilemania/Assets/Scripts/EnemyMovement.cs
+++ tilemania/Assets/Scripts/EnemyMovement.cs
@@ -3,33 +3,27 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
-    private Rigidbody2D _myRigidBody2D; 
-    
-    void Start()
+    private Rigidbody2D _myRigidBody2D;
+
+    private void Start()
     {
         _myRigidBody2D =  GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    private void Update()
     {
         _myRigidBody2D.linearVelocityX = moveSpeed;
     }
     
     private void OnTriggerExit2D(Collider2D other)
     {
+        if (other.CompareTag("Bullet")) return;
         moveSpeed = -moveSpeed;
         FlipEnemyFacing();
     }
-
-    // private void OnTriggerEnter2D(Collider2D other)
-    // {
-    //     moveSpeed = -moveSpeed;
-    //     FlipEnemyFacing();
-    // }
-
+    
     private void FlipEnemyFacing()
     {
         transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
-        // transform.localScale = new Vector2(-Mathf.Sign(_myRigidBody2D.linearVelocityX), 1F);
     }
 }
